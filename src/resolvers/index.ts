3f52e0eab7af741c7a8e6bfbda022911876ba1e6
Alfat Jahan Rony <34067640+alfatcse@users.noreply.github.com>
2023-12-04 23:13:50 +0100

    mutation done

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,10 +1,21 @@
-// Resolvers define how to fetch the types defined in your schema.
-// This resolver retrieves books from the "books" array ab ove.
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+interface userInfo {
+  name: string;
+  email: string;
+  password: string;
+}
 export const resolvers = {
-  Query: {},
+  Query: {
+    users: async (parent: any, args: any, context: any) => {
+      return await prisma.user.findMany();
+    },
+  },
   Mutation: {
-    signup: (parent: any, args: any, context: any) => {
-      console.log(args);
+    signup: async (parent: any, args: userInfo, context: any) => {
+      return await prisma.user.create({
+        data: args,
+      });
     },
   },
 };
